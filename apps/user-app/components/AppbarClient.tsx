cde8ada1d5fac041d8d78d6a94c380e36077805c
Ankit Raj <kumarankitraj1478@gmail.com>
2024-06-18 05:00:14 +0530

    fixed bug

--- apps/user-app/components/AppbarClient.tsx
+++ apps/user-app/components/AppbarClient.tsx
@@ -1,22 +1,18 @@
-"use client";
+"use client"
 import { signIn, signOut, useSession } from "next-auth/react";
 import { Appbar } from "@repo/ui/appbar";
 import { useRouter } from "next/navigation";
 
 export function AppbarClient() {
-  const { data: session } = useSession();
+  const session = useSession();
   const router = useRouter();
 
   return (
     <div>
-      <Appbar
-        onSignin={signIn}
-        onSignout={async () => {
-          await signOut({ callbackUrl: "/frontpage" });
-          router.push("/frontpage");
-        }}
-        user={session?.user}
-      />
+      <Appbar onSignin={signIn} onSignout={async () => {
+        await signOut()
+        router.push("/api/auth/signin")
+      }} user={session.data?.user} />
     </div>
   );
 }
